acf462a204cacda8e0c837b372cd7b5ecaee2591
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-03-31 15:09:08 +0100

    Module bootstrap and packager

---
+++ Module.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OmekaModuleStarterKit;
+
+use LogicException;
+use Omeka\Entity\Job;
+use Omeka\Module\AbstractModule;
+use Zend\Config\Reader\Yaml;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class Module extends AbstractModule {
+  public function getConfig() {
+    if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+      require_once __DIR__ . '/vendor/autoload.php';
+    }
+    if (file_exists(__DIR__ . '/config/module.config.yaml')) {
+      $yaml = new Yaml();
+      return $yaml->fromFile('/config/module.config.yaml');
+    }
+    if (file_exists(__DIR__ . '/config/module.config.php')) {
+      return include __DIR__ . '/config/module.config.php';
+    }
+    throw new LogicException('Configuration not found.');
+  }
+
+  public function install(ServiceLocatorInterface $serviceLocator) {
+    $connection        = $serviceLocator->get('Omeka\Connection');
+    $sqlToRunOnInstall = file_get_contents(__DIR__ . '/config/install.sql');;
+    if ($sqlToRunOnInstall) {
+      $connection->exec($sqlToRunOnInstall);
+    }
+  }
+
+  public function uninstall(ServiceLocatorInterface $serviceLocator) {
+    $connection          = $serviceLocator->get('Omeka\Connection');
+    $sqlToRunOnUninstall = file_get_contents(__DIR__ . '/config/uninstall.sql');;
+    if ($sqlToRunOnUninstall) {
+      $connection->exec($sqlToRunOnUninstall);
+    }
+  }
+}
