913ca161f3c70a353fb8d4ae4f28f894a5a5e4e7
stephenwf <stephen.fraser@digirati.com>
2017-09-27 18:19:21 +0100

    Clean up and bring up to date

--- Module.php
+++ Module.php
@@ -3,18 +3,17 @@
 namespace OmekaModuleStarterKit;
 
 use Omeka\Module\AbstractModule;
-use Symfony\Component\Yaml\Yaml as SymfonyYaml;
 use Zend\Config\Factory;
-use Zend\Config\Reader\Yaml as YamlConfig;
-use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class Module extends AbstractModule implements DependencyIndicatorInterface
+class Module extends AbstractModule
 {
     private $config;
 
     public function loadVendor() {
-        if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+        if (file_exists(__DIR__ . '/build/vendor-dist/autoload.php')) {
+            require_once __DIR__ . '/build/vendor-dist/autoload.php';
+        } elseif (file_exists(__DIR__ . '/vendor/autoload.php')) {
             require_once __DIR__ . '/vendor/autoload.php';
         }
     }
@@ -27,36 +26,9 @@ class Module extends AbstractModule implements DependencyIndicatorInterface
         // Load our composer dependencies.
         $this->loadVendor();
         // Load our configuration.
-        Factory::registerReader('yaml', new YamlConfig([SymfonyYaml::class, 'parse']));
         $this->config = Factory::fromFiles(
             glob(__DIR__ . '/config/*.config.*')
         );
         return $this->config;
-    }
-
-    public function install(ServiceLocatorInterface $serviceLocator)
-    {
-        $connection = $serviceLocator->get('Omeka\Connection');
-        $sqlToRunOnInstall = file_get_contents(__DIR__ . '/config/sql/install.sql');;
-        if ($sqlToRunOnInstall) {
-            $connection->exec($sqlToRunOnInstall);
-        }
-    }
-
-    public function uninstall(ServiceLocatorInterface $serviceLocator)
-    {
-        $connection = $serviceLocator->get('Omeka\Connection');
-        $sqlToRunOnUninstall = file_get_contents(__DIR__ . '/config/sql/uninstall.sql');;
-        if ($sqlToRunOnUninstall) {
-            $connection->exec($sqlToRunOnUninstall);
-        }
-    }
-
-    public function getModuleDependencies()
-    {
-        // You can use this to enforce Twig in an installation if its required.
-        // See open bug (https://github.com/omeka/omeka-s/issues/868)
-        // return ['ZfcTwig'];
-        return [];
     }
 }
